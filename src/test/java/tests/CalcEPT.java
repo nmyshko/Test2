aa276f0f2740070fd6f6cb9ead208abff9b79fd9
Nastya Myshko <nmyshko96@mail.ru>
2022-12-07 12:00:58 +0300

    selenium homework1

---
+++ src/test/java/tests/CalcEPT.java
@@ -0,0 +1,54 @@
+package tests;
+
+import factory.BrowserFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CalcEPT {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        BrowserFactory browserFactory = new BrowserFactory();
+        driver = browserFactory.getDriver();
+    }
+
+    @Test
+    public void calcEPT() throws InterruptedException {
+        driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
+        WebElement widthInput = driver.findElement(By.id("el_f_width"));
+        widthInput.sendKeys("3");
+        WebElement lengthInput = driver.findElement(By.id("el_f_lenght"));
+        lengthInput.sendKeys("4");
+        WebElement selectWebElement = driver.findElement(By.id("room_type"));
+        Select selectRoom = new Select(selectWebElement);
+        selectRoom.selectByValue("3");
+        selectRoom.selectByValue("4");
+        Thread.sleep(3000);
+        WebElement selectWebElement1 = driver.findElement(By.id("heating_type"));
+        Select selectHeatingType = new Select(selectWebElement1);
+        selectHeatingType.selectByValue("2");
+        selectHeatingType.selectByValue("3");
+        Thread.sleep(3000);
+        WebElement heatLossInput = driver.findElement(By.id("el_f_losses"));
+        heatLossInput.sendKeys("2500");
+        WebElement calcButton = driver.findElement(By.className("buttHFcalc"));
+        Thread.sleep(3000);
+        calcButton.click();
+        Assert.assertEquals(driver.findElement(By.linkText("Мощность нагревательного кабеля или мата, Вт")).getText(),
+                "Мощность нагревательного кабеля или мата, Вт");
+        Assert.assertEquals(driver.findElement(By.linkText("Удельная мощность нагревательного кабеля или мата, Вт/м")).getText(),
+                "Удельная мощность нагревательного кабеля или мата, Вт/м");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
