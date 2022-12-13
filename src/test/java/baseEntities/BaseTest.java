b861f624dfef6dacd32d87fd3f5095e0151e4157
Nastya Myshko <nmyshko96@mail.ru>
2022-12-14 00:12:07 +0300

    PageObject

---
+++ src/test/java/baseEntities/BaseTest.java
@@ -0,0 +1,27 @@
+package baseEntities;
+
+import configuration.ReadProperties;
+import factory.BrowserFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import steps.UserStep;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected UserStep userStep;
+    @BeforeMethod
+    public void setUp() {
+        driver = new BrowserFactory().getDriver();
+        driver.get(ReadProperties.getUrl());
+
+        userStep = new UserStep(driver);
+
+        System.out.println(driver.hashCode());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
